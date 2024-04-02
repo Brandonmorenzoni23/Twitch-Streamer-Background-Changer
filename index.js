de496350f266661a6349c3fdc65c3332f697a92b
Brandon Morenzoni <111790910+Brandonmorenzoni23@users.noreply.github.com>
2024-04-02 10:07:49 -0500

    Background Changer

---
+++ index.js
@@ -0,0 +1,37 @@
+document.getElementById('black').addEventListener('click', black)
+document.getElementById('blue').addEventListener('click', blue)
+document.getElementById('green').addEventListener('click', green)
+
+
+function black(){
+    console.log('happy')
+    document.getElementById('color').style.background= 'black'
+
+}
+function blue(){
+    console.log('happy')
+    document.getElementById('color').style.background = 'blue'
+  
+
+}
+function green(){
+    console.log('happy')
+    document.getElementById('color').style.background = 'green'
+
+}
+
+
+
+
+
+
+
+//const color = document.querySelectorAll('.event').forEach(element => {
+ //   element.addEventListener('click', Changebackground)
+//});
+
+
+//Changebackground(){
+
+//}
+
